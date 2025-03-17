42b50304dd14ab273b099efaafff55c5892250e6
Oye Olalekan Johnson <johnsonoye34@gmail.com>
2025-03-17 08:38:13 +0100

    feat: add seo image

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,25 @@ export const metadata: Metadata = {
     "website SEO checker",
     "structured data testing tool",
     "free meta tag checker"
-  ]
+  ],
+  openGraph: {
+    title: "MetaCheck – Easily preview your website’s OG image, meta tags, favicon, and more.",
+    description: "Ensure your website links display perfectly on social media and messaging apps with MetaCheck. Analyze and preview your OG image, meta tags, favicon, and more for free. Optimize your link previews for better engagement and click-through rates.",
+    images: [
+      {
+        url: "/seo-image.png",
+        width: 1200,
+        height: 630,
+        alt: "MetaCheck – Easily preview your website’s OG image, meta tags, favicon, and more.",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SweetMetaCheck – Easily preview your website’s OG image, meta tags, favicon, and more.otes",
+    description: "Ensure your website links display perfectly on social media and messaging apps with MetaCheck. Analyze and preview your OG image, meta tags, favicon, and more for free. Optimize your link previews for better engagement and click-through rates.",
+    images: ["/seo-image.png"],
+  },
 };
 
 export default function RootLayout({
